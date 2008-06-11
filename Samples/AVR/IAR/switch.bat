b598e7412e56b5dd9f21f7ba1a795d043f02c6d1
Harry E. Zhurov <harry.zhurov@gmail.com>
2008-06-11 08:27:54 +0000

    Added command files (switch.bat) to every Sample for automatic release creation.

---
+++ Samples/AVR/IAR/switch.bat
@@ -0,0 +1,11 @@
+@echo off
+
+SET REP=https://scmrtos.svn.sourceforge.net/svnroot/scmrtos/trunk
+
+SET TARGET=AVR
+SET TOOL=IAR
+SET DST_DIR=.
+
+svn switch  %REP%/Common/                   %DST_DIR%/scmRTOS/Common
+svn switch  %REP%/Ports/%TARGET%/%TOOL%/    %DST_DIR%/scmRTOS/%TARGET%
+
---
+++ Samples/FR/Softune_V6/switch.bat
@@ -0,0 +1,10 @@
+@echo off
+
+SET REP=https://scmrtos.svn.sourceforge.net/svnroot/scmrtos/trunk
+SET TARGET=FR
+SET TOOL=Softune_V6
+SET DST_DIR=.
+
+svn switch  %REP%/Common/                   %DST_DIR%/scmRTOS/Common
+svn switch  %REP%/Ports/%TARGET%/%TOOL%/    %DST_DIR%/scmRTOS/%TARGET%
+
