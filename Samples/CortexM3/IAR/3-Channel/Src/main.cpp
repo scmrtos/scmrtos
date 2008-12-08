c272442bbaa9a0d032e7c4255fcb3fae98cba0f5
Andrey V. Chuikin <avct@termexlab.ru>
2008-12-08 10:05:35 +0000

    Первая фиксация порта (и сэмплов) scmRTOS для Cortex-M3. Особенности порта. 1. Для переключения контекста используется только программное прерывание - PendSV (как самый "родной" метод).    Прямой метод переключения не реализован. 2. В качестве системного таймера используется System tick timer. 3. Настройку таймера выполняет ОС, за исключением настройки частоты его клока, что должен сделать пользователь.    Для настройки SysTick'a в scmRTOS_TARGET_CFG.h следует указать частоту клока таймера и желаемую частоту таймерного прерывания. 4. Поскольку вложенные прерывания реализуются аппаратно, то макрос ENABLE_NESTED_INTERRUPTS пустой.    Дополнительно введен макрос DISABLE_NESTED_INTERRUPTS для запрета вложенных прерываний. 5. Наиболее эффективное переключение контекста выполняется при scmRTOS_PRIORITY_ORDER==1. 6. Прерывания используют Main_stack. Программа (задачи) - Process_stack. 7. Не забываем в таблице векторов прерываний (__vector_table[]) указать на соответствующих местах    2 обработчика - PendSVC_ISR, SystemTimer_ISR.

---
+++ Samples/CortexM3/IAR/3-Channel/Src/main.cpp
@@ -0,0 +1,160 @@
+//******************************************************************************
+//*
+//*     FULLNAME:  Single-Chip Microcontroller Real-Time Operating System
+//*
+//*     NICKNAME:  scmRTOS
+//*
+//*     PROCESSOR: ARM Cortex-M3 
+//*
+//*     TOOLKIT:   EWARM (IAR Systems)
+//*
+//*     PURPOSE:   Port Test File
+//*
+//*     Version: 3.05
+//*
+//*     $Revision: 196 $
+//*     $Date:: 2008-06-19 #$
+//*
+//*     Copyright (c) 2003-2008, Harry E. Zhurov
+//*
+//*     Permission is hereby granted, free of charge, to any person 
+//*     obtaining  a copy of this software and associated documentation 
+//*     files (the "Software"), to deal in the Software without restriction, 
+//*     including without limitation the rights to use, copy, modify, merge, 
+//*     publish, distribute, sublicense, and/or sell copies of the Software, 
+//*     and to permit persons to whom the Software is furnished to do so, 
+//*     subject to the following conditions:
+//*
+//*     The above copyright notice and this permission notice shall be included 
+//*     in all copies or substantial portions of the Software.
+//*
+//*     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+//*     EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+//*     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+//*     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+//*     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+//*     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
+//*     THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//*
+//*     =================================================================
+//*     See http://scmrtos.sourceforge.net for documentation, latest
+//*     information, license and contact details.
+//*     =================================================================
+//*
+//******************************************************************************
+//*     Ported by Andrey Chuikin, Copyright (c) 2008
+
+#include <scmRTOS.h>
+
+//---------------------------------------------------------------------------
+//
+//      Process types
+//
+typedef OS::process<OS::pr0, 400> TProc1;
+typedef OS::process<OS::pr1, 400> TProc2;
+typedef OS::process<OS::pr2, 400> TProc3;
+//---------------------------------------------------------------------------
+//
+//      Process objects
+//
+TProc1 Proc1;
+TProc2 Proc2;
+TProc3 Proc3;
+//---------------------------------------------------------------------------
+dword T;                         // global variable for OS::GetTickCount testing
+                                 //
+
+//------------------------------------------------------------------------------
+//
+//   Message "body"
+// 
+// 
+class TSlon
+{
+public:
+    TSlon() { }
+    virtual void eat() = 0;      // feed the slon. For non-russians: slon == elephant ;)
+};
+//------------------------------------------------------------------------------
+class TAfricanSlon : public TSlon
+{
+public:
+    virtual void eat()
+    {
+        TCritSect cs; 
+
+        ; // Make some action
+    }
+};
+//------------------------------------------------------------------------------
+class TIndianSlon : public TSlon
+{
+public:
+    virtual void eat()
+    {
+        TCritSect cs; 
+
+        ; // Make some action
+    }
+};
+//------------------------------------------------------------------------------
+
+TAfricanSlon African;   
+TIndianSlon  Indian;    
+    
+OS::channel<TSlon*, 8> SlonQueue; // OS::channel object for 8 'TSlon*' items
+
+OS::TEventFlag Timer_B_Ovf;
+
+//---------------------------------------------------------------------------
+void main()
+{
+    OS::Run();
+}
+//---------------------------------------------------------------------------
+OS_PROCESS void TProc1::Exec()
+{
+    for(;;)
+    {
+        Timer_B_Ovf.Wait();
+        SlonQueue.push(&African);
+    }     
+}
+//---------------------------------------------------------------------------
+OS_PROCESS void TProc2::Exec()
+{
+    for(;;)
+    {
+        T += OS::GetTickCount();
+             
+        Sleep(1);
+        SlonQueue.push(&Indian);
+    }
+}
+//---------------------------------------------------------------------------
+OS_PROCESS void TProc3::Exec()
+{
+    for(;;)
+    {
+        //--------------------------------------------------
+        //
+        //            Channel test
+        //
+        //
+        //     Get data through channel
+        //
+        TSlon *p;
+        SlonQueue.pop(p);     // get pointer from queue
+        p->eat();             // feed the appropriate Slon
+    }
+}
+//---------------------------------------------------------------------------
+void OS::SystemTimerUserHook()
+{
+    Timer_B_Ovf.SignalISR();
+}
+//---------------------------------------------------------------------------
+void OS::IdleProcessUserHook() { }
+//---------------------------------------------------------------------------
+
+
