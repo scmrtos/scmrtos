81d47af2816af42140dd5f4d5929ff56b0fae183
Harry E. Zhurov <harry.zhurov@gmail.com>
2012-03-30 04:49:39 +0000

    Trunk contents (v3) moved to branch v3xx.

--- Samples/FR/Softune_V6/MB91270/mb91270.c
+++
@@ -1,9 +0,0 @@
-/* THIS SAMPLE CODE IS PROVIDED AS IS AND IS SUBJECT TO ALTERATIONS. FUJITSU */
-/* MICROELECTRONICS ACCEPTS NO RESPONSIBILITY OR LIABILITY FOR ANY ERRORS OR */
-/* ELIGIBILITY FOR ANY PURPOSES.                                             */
-/*                 (C) Fujitsu Microelectronics Europe GmbH                  */
-/*---------------------------------------------------------------------------*/
-
-
-#define __IO_DEFINE
-#include "MB91270.h"
--- Samples/MSP430/GCC/get_common.bat
+++
@@ -1,4 +0,0 @@
-SET REP=http://scmrtos.svn.sourceforge.net/svnroot/scmrtos/trunk
-
-svn switch %REP%/Common/ ./scmRTOS/Common
-svn switch %REP%/Ports/MSP430/GCC ./scmRTOS/MSP430
