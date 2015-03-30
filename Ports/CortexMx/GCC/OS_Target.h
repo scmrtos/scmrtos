a718a881c7315bb80afceade53e2ccceafeefffc
Anton Gusev <antongus@mail.ru>
2015-03-30 12:06:05 +0000

    Added universal CortexMx port

---
+++ Ports/CortexMx/GCC/OS_Target.h
@@ -0,0 +1,322 @@
+//******************************************************************************
+//*
+//*     FULLNAME:  Single-Chip Microcontroller Real-Time Operating System
+//*
+//*     NICKNAME:  scmRTOS
+//*
+//*     PROCESSOR: ARM Cortex-M0(+), Cortex-M1, Cortex-M3, Cortex-M4(F)
+//*
+//*     TOOLKIT:   ARM GCC
+//*
+//*     PURPOSE:   Target Dependent Stuff Header. Declarations And Definitions
+//*
+//*     Version: 4.00
+//*
+//*     $Revision$
+//*     $Date::             $
+//*
+//*     Copyright (c) 2003-2012, Harry E. Zhurov
+//*
+//*     Permission is hereby granted, free of charge, to any person
+//*     obtaining  a copy of this software and associated documentation
+//*     files (the "Software"), to deal in the Software without restriction,
+//*     including without limitation the rights to use, copy, modify, merge,
+//*     publish, distribute, sublicense, and/or sell copies of the Software,
+//*     and to permit persons to whom the Software is furnished to do so,
+//*     subject to the following conditions:
+//*
+//*     The above copyright notice and this permission notice shall be included
+//*     in all copies or substantial portions of the Software.
+//*
+//*     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//*     EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//*     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//*     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//*     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//*     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+//*     THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//*
+//*     =================================================================
+//*     See http://scmrtos.sourceforge.net for documentation, latest
+//*     information, license and contact details.
+//*     =================================================================
+//*
+//******************************************************************************
+//*     Cortex-M3/M4(F) GCC port by Anton B. Gusev aka AHTOXA, Copyright (c) 2012
+//*     Cortex-M0 port by Sergey A. Borshch, Copyright (c) 2011
+
+#ifndef scmRTOS_CORTEXM4_H
+#define scmRTOS_CORTEXM4_H
+
+//------------------------------------------------------------------------------
+//
+//    Compiler and Target checks
+//
+//
+#ifndef __GNUC__
+#error "This file should only be compiled with GNU C++ Compiler"
+#endif // __GNUC__
+
+#if (!defined __ARM_ARCH_7M__) && (!defined __ARM_ARCH_7EM__) && (!defined __ARM_ARCH_6M__)
+#error "This file must be compiled for ARMv6-M (Cortex-M0(+)), ARMv7-M (Cortex-M3) and ARMv7E-M (Cortex-M4(F)) processors only."
+#endif
+
+#if (__GNUC__ < 3)
+#error "This file must be compiled by GCC C/C++ Compiler v3.0 or higher."
+#endif
+
+//------------------------------------------------------------------------------
+//
+//    Compiler specific attributes
+//
+//
+#ifndef INLINE
+#define INLINE      __attribute__((__always_inline__)) inline
+#endif
+
+#ifndef NOINLINE
+#define NOINLINE    __attribute__((__noinline__))
+#endif
+
+#ifndef NORETURN
+#define NORETURN    __attribute__((__noreturn__))
+#endif
+
+//------------------------------------------------------------------------------
+//
+//    Target specific types
+//
+//
+typedef uint32_t stack_item_t;
+typedef uint32_t status_reg_t;
+
+//-----------------------------------------------------------------------------
+//
+//    Configuration macros
+//
+//
+#define OS_PROCESS __attribute__((__noreturn__))
+#define OS_INTERRUPT extern "C"
+
+#define DUMMY_INSTR() __asm__ __volatile__ ("nop")
+#define INLINE_PROCESS_CTOR INLINE
+
+//-----------------------------------------------------------------------------
+// Separate return stack not required
+#define SEPARATE_RETURN_STACK   0
+
+//-----------------------------------------------------------------------------
+// Software interrupt stack switching not supported in Cortex-M port
+// because processor implements hardware stack switching.
+// So, system timer ISR wrapper can't be chosen at project level
+//
+#define scmRTOS_ISRW_TYPE       TISRW
+
+//-----------------------------------------------------------------------------
+//
+//    scmRTOS Context Switch Scheme
+//
+//    The macro defines a context switch manner. Value 0 sets direct context
+//    switch in the scheduler and in the OS ISRs. This is the primary method.
+//    Value 1 sets the second way to switch context - by using of software
+//    interrupt. See documentation for details.
+//    Cortex-M port supports software interrupt switch method only.
+//
+#define  scmRTOS_CONTEXT_SWITCH_SCHEME 1
+
+//-----------------------------------------------------------------------------
+//
+//    scmRTOS Priority Order
+//
+//    This macro defines the order of the process's priorities. Default,
+//    the ascending order is used. Alternatively, the descending priority
+//    order can be used. On some platforms the descending order is preferred
+//    because of performance.
+//
+//    Default (corresponding to ascending order) value of macro is 0.
+//    Alternative (corresponding to descending order) value of macro is 1.
+//
+//    On Cortex-M3/M4 the descending order is used for performance reason.
+//    Cortex-M0 lacks CLZ instruction, so only ascending order is implemented for it.
+//
+#if (defined __ARM_ARCH_6M__)
+#define  scmRTOS_PRIORITY_ORDER             0
+#else
+#define  scmRTOS_PRIORITY_ORDER             1
+#endif
+
+//-----------------------------------------------------------------------------
+//
+//     Include project-level configurations
+//    !!! The order of includes is important !!!
+//
+#include "scmRTOS_CONFIG.h"
+#include "scmRTOS_TARGET_CFG.h"
+#include <scmRTOS_defs.h>
+
+//-----------------------------------------------------------------------------
+//
+//    Target-specific configuration macros
+//
+#ifdef scmRTOS_USER_DEFINED_STACK_PATTERN
+#define scmRTOS_STACK_PATTERN scmRTOS_USER_DEFINED_STACK_PATTERN
+#else
+#define scmRTOS_STACK_PATTERN 0xABBA
+#endif
+
+
+//-----------------------------------------------------------------------------
+//
+//     Interrupt and Interrupt Service Routines support
+//
+#define enable_interrupts() __asm__ __volatile__ ("cpsie i")
+#define disable_interrupts() __asm__ __volatile__ ("cpsid i")
+
+INLINE void set_interrupt_state(status_reg_t status)
+{
+    __asm__ __volatile__ (
+        "MSR PRIMASK, %0\n"
+        : : "r"(status)
+        :"memory"
+    );
+}
+
+INLINE status_reg_t get_interrupt_state()
+{
+    status_reg_t sr;
+    __asm__ __volatile__ ("MRS %0, PRIMASK" : "=r"(sr) );
+    return sr;
+}
+
+#if scmRTOS_OBSOLETE_NAMES == 1
+
+INLINE status_reg_t GetInterruptState( )       { return get_interrupt_state(); }
+INLINE void SetInterruptState(status_reg_t sr) { set_interrupt_state(sr);      }
+
+INLINE void EnableInterrupts()  { enable_interrupts();  }
+INLINE void DisableInterrupts() { disable_interrupts(); }
+
+#endif // scmRTOS_OBSOLETE_NAMES
+
+
+//-----------------------------------------------------------------------------
+//
+//     The Critical Section Wrapper
+//
+//
+#if scmRTOS_USER_DEFINED_CRITSECT_ENABLE == 0
+class TCritSect
+{
+public:
+    TCritSect () : StatusReg(get_interrupt_state()) { disable_interrupts(); }
+    ~TCritSect() { set_interrupt_state(StatusReg); }
+
+private:
+    status_reg_t StatusReg;
+};
+#endif // scmRTOS_USER_DEFINED_CRITSECT_ENABLE
+
+//   Uncomment macro value below for system_timer() and
+//   context_switch_hook() run in critical section.
+// 
+//   This is useful (and necessary) when target processor has hardware 
+//   enabled nested interrupts.
+//   User can define own macros using user-defined TCritSect capabilities.
+//
+//   Cortex-M have nested interrupts but interrupts are disabled
+//   during context switch ISR. So, critical section is needed
+//   for system timer routine and not needed for context switcher.
+// 
+#define SYS_TIMER_CRIT_SECT() TCritSect cs
+#define CONTEXT_SWITCH_HOOK_CRIT_SECT()
+
+
+//-----------------------------------------------------------------------------
+//
+//     Priority stuff
+//
+//
+namespace OS
+{
+INLINE OS::TProcessMap get_prio_tag(const uint_fast8_t pr) { return static_cast<OS::TProcessMap> (1 << pr); }
+
+#if scmRTOS_PRIORITY_ORDER == 0
+    INLINE uint_fast8_t highest_priority(TProcessMap pm)
+    {
+        extern TPriority const PriorityTable[];
+
+        #if scmRTOS_PROCESS_COUNT < 6
+            return PriorityTable[pm];
+        #else
+            uint32_t x = pm;
+            x = x & -x;                             // Isolate rightmost 1-bit.
+
+                                                // x = x * 0x450FBAF
+            x = (x << 4) | x;                       // x = x*17.
+            x = (x << 6) | x;                       // x = x*65.
+            x = (x << 16) - x;                      // x = x*65535.
+
+            return PriorityTable[x >> 26];
+        #endif  // scmRTOS_PROCESS_COUNT < 6
+    }
+#else
+    INLINE uint_fast8_t highest_priority(TProcessMap pm)
+    {
+    	return 31 - __builtin_clz(pm);
+    }
+#endif // scmRTOS_PRIORITY_ORDER
+}
+
+namespace OS
+{
+    INLINE void enable_context_switch()  { enable_interrupts(); }
+    INLINE void disable_context_switch() { disable_interrupts(); }
+}
+
+#include <OS_Kernel.h>
+
+namespace OS
+{
+    //--------------------------------------------------------------------------
+    //
+    //      NAME       :   OS ISR support
+    //
+    //      PURPOSE    :   Implements common actions on interrupt enter and exit
+    //                     under the OS
+    //
+    //      DESCRIPTION:
+    //
+    //
+    class TISRW
+    {
+    public:
+        INLINE  TISRW()  { ISR_Enter(); }
+        INLINE  ~TISRW() { ISR_Exit();  }
+
+    private:
+        //-----------------------------------------------------
+        INLINE void ISR_Enter()
+        {
+            TCritSect cs;
+            Kernel.ISR_NestCount++;
+        }
+        //-----------------------------------------------------
+        INLINE void ISR_Exit()
+        {
+            TCritSect cs;
+            if(--Kernel.ISR_NestCount) return;
+            Kernel.sched_isr();
+        }
+        //-----------------------------------------------------
+    };
+
+    // No software interrupt stack switching provided,
+    // TISRW_SS declared to be the same as TISRW for porting compatibility
+    #define TISRW_SS    TISRW
+
+} // namespace OS
+//-----------------------------------------------------------------------------
+
+#endif // scmRTOS_CORTEXM4_H
+//-----------------------------------------------------------------------------
+
---
+++ Ports/CortexMx/GCC/OS_Target_cpp.cpp
@@ -0,0 +1,383 @@
+//******************************************************************************
+//*
+//*     FULLNAME:  Single-Chip Microcontroller Real-Time Operating System
+//*
+//*     NICKNAME:  scmRTOS
+//*
+//*     PROCESSOR: ARM Cortex-M0(+), Cortex-M1, Cortex-M3, Cortex-M4(F)
+//*
+//*     TOOLKIT:   ARM GCC
+//*
+//*     PURPOSE:   Target Dependent Stuff Source
+//*
+//*     Version: 4.00
+//*
+//*     $Revision$
+//*     $Date::             $
+//*
+//*     Copyright (c) 2003-2012, Harry E. Zhurov
+//*
+//*     Permission is hereby granted, free of charge, to any person
+//*     obtaining  a copy of this software and associated documentation
+//*     files (the "Software"), to deal in the Software without restriction,
+//*     including without limitation the rights to use, copy, modify, merge,
+//*     publish, distribute, sublicense, and/or sell copies of the Software,
+//*     and to permit persons to whom the Software is furnished to do so,
+//*     subject to the following conditions:
+//*
+//*     The above copyright notice and this permission notice shall be included
+//*     in all copies or substantial portions of the Software.
+//*
+//*     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//*     EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//*     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//*     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//*     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//*     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+//*     THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//*
+//*     =================================================================
+//*     See http://scmrtos.sourceforge.net for documentation, latest
+//*     information, license and contact details.
+//*     =================================================================
+//*
+//******************************************************************************
+//*     Cortex-M3/M4(F) GCC port by Anton B. Gusev aka AHTOXA, Copyright (c) 2012
+//*     Cortex-M0 port by Sergey A. Borshch, Copyright (c) 2011
+
+
+#include <scmRTOS.h>
+
+/*
+ * M0(+) core : __ARM_ARCH_6M__ defined.
+ * M3 core    : __SOFTFP__ defined, __ARM_ARCH_6M__ not defined.
+ * M4F core   : __SOFTFP__ not defined.
+ */
+
+using namespace OS;
+
+namespace{
+enum {
+#if (defined __SOFTFP__)
+    CONTEXT_SIZE = 16 * sizeof(stack_item_t)   // Cortex-M0/M0+/M3/M4 context size
+#else
+    CONTEXT_SIZE = 17 * sizeof(stack_item_t)   // Cortex-M4F initial context size (without FPU context)
+#endif
+};
+}
+
+void TBaseProcess::init_stack_frame( stack_item_t * Stack
+                                   , void (*exec)()
+                                #if scmRTOS_DEBUG_ENABLE == 1
+                                   , stack_item_t * StackBegin
+                                #endif
+                                   )
+{
+    /*
+     * ARM Architecture Procedure Call Standard [AAPCS] requires 8-byte stack alignment.
+     * This means that we must get top of stack aligned _after_ context "pushing", at
+     * interrupt entry.
+     */
+    uintptr_t sptr = (((uintptr_t)Stack - CONTEXT_SIZE) & 0xFFFFFFF8UL) + CONTEXT_SIZE;
+    StackPointer = (stack_item_t*)sptr;
+
+    *(--StackPointer)  = 0x01000000UL;      // xPSR
+    *(--StackPointer)  = reinterpret_cast<uint32_t>(exec); // Entry Point
+#if (defined __SOFTFP__)    // core without FPU
+    StackPointer -= 14;                     // emulate "push LR,R12,R3,R2,R1,R0,R11-R4"
+#else                       // core with FPU
+    StackPointer -= 6;                      // emulate "push LR,R12,R3,R2,R1,R0"
+    *(--StackPointer)  = 0xFFFFFFFDUL;      // exc_return: Return to Thread mode, floating-point context inactive, execution uses PSP after return.
+    StackPointer -= 8;                      // emulate "push R4-R11"
+#endif
+
+#if scmRTOS_DEBUG_ENABLE == 1
+    for (stack_item_t* pDst = StackBegin; pDst < StackPointer; pDst++)
+        *pDst = STACK_DEFAULT_PATTERN;
+#endif // scmRTOS_DEBUG_ENABLE
+}
+
+/*
+ * PendSV is used to perform a context switch. This is a recommended method for Cortex-M.
+ * This is because the Cortex-M automatically saves half of the processor context
+ * on any exception, and restores same on return from exception.  So only saving of R4-R11
+ * and fixing up the stack pointers is required.  Using the PendSV exception this way means
+ * that context saving and restoring is identical whether it is initiated from a thread
+ * or occurs due to an interrupt or exception.
+ *
+ * Since PendSV interrupt has the lowest priority in the system (set by os_start() below),
+ * we can be sure that it will run only when no other exception or interrupt is active, and
+ * therefore safe to assume that context being switched out was using the process stack (PSP).
+ */
+extern "C" void PendSV_Handler()
+{
+#if (defined __ARM_ARCH_6M__)   // Cortex-M0(+)/Cortex-M1
+    asm volatile (
+        "    CPSID   I                 \n"  // Prevent interruption during context switch
+        "    MRS     R0, PSP           \n"  // Load process stack pointer to R0
+        "    SUB     R0, R0, #32       \n"  // Adjust R0 to point to top of saved context in stack
+        "    MOV     R1, R0            \n"  // Preserve R0 (needed for os_context_switch_hook() call)
+        "    STMIA   R1!, {R4-R7}      \n"  // Save low portion of remaining registers (r4-7) on process stack
+        "    MOV     R4, R8            \n"  // Move high portion of remaining registers (r8-11) to low registers
+        "    MOV     R5, R9            \n"
+        "    MOV     R6, R10           \n"
+        "    MOV     R7, R11           \n"
+        "    STMIA   R1!, {R4-R7}      \n"  // Save high portion of remaining registers (r8-11) on process stack
+
+        // At this point, entire context of process has been saved
+        "    PUSH    {LR}              \n" // we must save LR (exc_return value) until exception return
+        "    BL      os_context_switch_hook  \n" // call os_context_switch_hook();
+
+        // R0 is new process SP;
+        "    ADD     R0, R0, #16       \n" // Adjust R0 to point to high registers (r8-11)
+        "    LDMIA   R0!, {R4-R7}      \n" // Restore r8-11 from new process stack
+        "    MOV     R8, R4            \n" // Move restored values to high registers (r8-11)
+        "    MOV     R9, R5            \n"
+        "    MOV     R10, R6           \n"
+        "    MOV     R11, R7           \n"
+        "    MSR     PSP, R0           \n" // R0 at this point is new process SP
+        "    SUB     R0, R0, #32       \n" // Adjust R0 to point to low registers
+        "    LDMIA   R0!, {R4-R7}      \n" // Restore r4-7
+        "    CPSIE   I                 \n"
+        "    POP     {PC}              \n" // Return to saved exc_return. Exception return will restore remaining context
+        : :
+    );
+#else  // #if (defined __ARM_ARCH_6M__)
+
+#if (defined __SOFTFP__)
+    // M3/M4 cores without FPU
+    asm volatile (
+        "    CPSID   I                 \n" // Prevent interruption during context switch
+        "    MRS     R0, PSP           \n" // PSP is process stack pointer
+        "    STMDB   R0!, {R4-R11}     \n" // Save remaining regs r4-11 on process stack
+
+        // At this point, entire context of process has been saved
+        "    PUSH    {LR}              \n" // we must save LR (exc_return value) until exception return
+        "    BL      os_context_switch_hook  \n" // call os_context_switch_hook();
+
+        // R0 is new process SP;
+        "    LDMIA   R0!, {R4-R11}     \n" // Restore r4-11 from new process stack
+        "    MSR     PSP, R0           \n" // Load PSP with new process SP
+        "    CPSIE   I                 \n"
+        "    POP     {PC}              \n" // Return to saved exc_return. Exception return will restore remaining context
+        : :
+    );
+
+#else // #if (defined __SOFTFP__)
+    // Core with FPU (cortex-M4F)
+    asm volatile (
+        "    CPSID     I                 \n" // Prevent interruption during context switch
+        "    MRS       R0, PSP           \n" // PSP is process stack pointer
+        "    TST       LR, #0x10         \n" // exc_return[4]=0? (it means that current process
+        "    IT        EQ                \n" // has active floating point context)
+        "    VSTMDBEQ  R0!, {S16-S31}    \n" // if so - save it.
+        "    STMDB     R0!, {R4-R11, LR} \n" // save remaining regs r4-11 and LR on process stack
+
+        // At this point, entire context of process has been saved
+        "    BL        os_context_switch_hook  \n" // call os_context_switch_hook();
+
+        // R0 is new process SP;
+        "    LDMIA     R0!, {R4-R11, LR} \n" // Restore r4-11 and LR from new process stack
+        "    TST       LR, #0x10         \n" // exc_return[4]=0? (it means that new process
+        "    IT        EQ                \n" // has active floating point context)
+        "    VLDMIAEQ  R0!, {S16-S31}    \n" // if so - restore it.
+        "    MSR       PSP, R0           \n" // Load PSP with new process SP
+        "    CPSIE     I                 \n"
+        "    BX        LR                \n" // Return to saved exc_return. Exception return will restore remaining context
+        : :
+    );
+#endif  // #if (defined __SOFTFP__)
+#endif  // #if (defined __ARM_ARCH_6M__)
+}
+
+// weak alias to support old name of PendSV_Handler.
+#pragma weak PendSVC_ISR = PendSV_Handler
+
+/*
+ * Some Cortex-MX registers and constants.
+ */
+namespace
+{
+
+template<uint32_t addr, typename type = uint32_t>
+struct ioregister_t
+{
+    type operator=(type value) { *(volatile type*)addr = value; return value; }
+    void operator|=(type value) { *(volatile type*)addr |= value; }
+    void operator&=(type value) { *(volatile type*)addr &= value; }
+    operator type() { return *(volatile type*)addr; }
+};
+
+template<uint32_t addr, class T>
+struct iostruct_t
+{
+    volatile T* operator->() { return (volatile T*)addr; }
+};
+
+// PendSV and SysTick priority registers
+static ioregister_t<0xE000ED22UL, uint8_t> PendSvPriority;
+static ioregister_t<0xE000ED23UL, uint8_t> SysTickPriority;
+
+// SysTick stuff
+struct systick_t
+{
+    uint32_t       CTRL;
+	uint32_t       LOAD;
+  	uint32_t       VAL;
+  	uint32_t const CALIB;
+};
+
+enum
+{
+    NVIC_ST_CTRL_CLK_SRC = 0x00000004,       // Clock Source.
+    NVIC_ST_CTRL_INTEN   = 0x00000002,       // Interrupt enable.
+    NVIC_ST_CTRL_ENABLE  = 0x00000001        // Counter mode.
+};
+
+static iostruct_t<0xE000E010UL, systick_t> SysTick;
+
+#if (!defined __SOFTFP__)
+// Floating point context control register stuff
+static ioregister_t<0xE000EF34UL> FPCCR;
+enum
+{
+    ASPEN =   (0x1UL << 31),  // always save enable
+    LSPEN =   (0x1UL << 30)   // lazy save enable
+};
+#endif
+
+}
+
+/*
+ * System timer stuff.
+ * This is the default (weak) system timer ISR handler.
+ * The user can redefine this handler if needed.
+ */
+#pragma weak SysTick_Handler = Default_SystemTimer_ISR
+#pragma weak SystemTimer_ISR = Default_SystemTimer_ISR
+
+namespace OS
+{
+extern "C" OS_INTERRUPT void Default_SystemTimer_ISR()
+{
+    scmRTOS_ISRW_TYPE ISR;
+
+#if scmRTOS_SYSTIMER_HOOK_ENABLE == 1
+    system_timer_user_hook();
+#endif
+
+    Kernel.system_timer();
+
+#if scmRTOS_SYSTIMER_NEST_INTS_ENABLE == 0
+    DISABLE_NESTED_INTERRUPTS();
+#endif
+}
+}
+
+/*
+ * Default system timer initialization.
+ *
+ * Sets SysTick timer interrupt priority to lowest.
+ * Configures SysTick timer to interrupt with frequency SYSTICKINTRATE.
+ */
+#pragma weak __init_system_timer
+extern "C" void __init_system_timer()
+{
+	SysTickPriority = 0xFF;
+	SysTick->LOAD = SYSTICKFREQ/SYSTICKINTRATE-1;
+	SysTick->CTRL = NVIC_ST_CTRL_CLK_SRC | NVIC_ST_CTRL_INTEN | NVIC_ST_CTRL_ENABLE;
+}
+
+
+/*
+ *   START MULTITASKING
+ *
+ * Note(s) : 1) os_start() MUST:
+ *              a) Setup PendSV and SysTick exception priority to lowest;
+ *              b) Call System timer initialization function;
+ *              c) Enable interrupts (tasks will run with interrupts enabled);
+ *              d) Jump to entry point of the highest priority process.
+ */
+extern "C" NORETURN void os_start(stack_item_t *sp)
+{
+	PendSvPriority = 0xFF;     // PendSV priority value (lowest)
+#if (!defined __SOFTFP__)
+    FPCCR |= ASPEN | LSPEN;    // move this to os_start
+#endif
+
+    asm volatile (
+#if (defined __SOFTFP__)  // code without FPU
+        "    LDR     R4, [%[stack], #(4 * 14)] \n" // Load process entry point into R4
+        "    ADD     %[stack], #(4 * 16)       \n" // emulate context restore
+#else
+    	"    LDR     R4, [%[stack], #(4 * 15)] \n" // Load process entry point into R4
+        "    ADD     %[stack], #(4 * 17)       \n" // emulate context restore
+#endif
+        "    MSR     PSP, %[stack]             \n" // store process SP to PSP
+        "    MOV     R0, #2                    \n" // set up the current (thread) mode: use PSP as stack pointer, privileged level
+        "    MSR     CONTROL, R0               \n"
+        "    ISB                               \n" // Insert a barrier
+   	    "    BL      __init_system_timer       \n" // run system timer
+   	    "    CPSIE   I                         \n" // Enable interrupts at processor level
+   	    "    BX      R4                        \n" // Jump to process exec() function
+        : [stack]"+r" (sp)  // output
+    );
+    __builtin_unreachable(); // suppress compiler warning "'noreturn' func does return"
+}
+
+#if scmRTOS_PRIORITY_ORDER == 0
+namespace OS
+{
+    extern TPriority const PriorityTable[] =
+    {
+        #if scmRTOS_PROCESS_COUNT == 1
+            (TPriority)0xFF,
+            pr0,
+            prIDLE, pr0
+        #elif scmRTOS_PROCESS_COUNT == 2
+            (TPriority)0xFF,
+            pr0,
+            pr1, pr0,
+            prIDLE, pr0, pr1, pr0
+        #elif scmRTOS_PROCESS_COUNT == 3
+            (TPriority)0xFF,
+            pr0,
+            pr1, pr0,
+            pr2, pr0, pr1, pr0,
+            prIDLE, pr0, pr1, pr0, pr2, pr0, pr1, pr0
+        #elif scmRTOS_PROCESS_COUNT == 4
+            (TPriority)0xFF,
+            pr0,
+            pr1, pr0,
+            pr2, pr0, pr1, pr0,
+            pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0,
+            prIDLE, pr0, pr1, pr0, pr2, pr0, pr1, pr0, pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0
+        #elif scmRTOS_PROCESS_COUNT == 5
+            (TPriority)0xFF,
+            pr0,
+            pr1, pr0,
+            pr2, pr0, pr1, pr0,
+            pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0,
+            pr4, pr0, pr1, pr0, pr2, pr0, pr1, pr0, pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0,
+            prIDLE, pr0, pr1, pr0, pr2, pr0, pr1, pr0, pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0, pr4, pr0, pr1, pr0, pr2, pr0, pr1, pr0, pr3, pr0, pr1, pr0, pr2, pr0, pr1, pr0
+        #else // scmRTOS_PROCESS_COUNT > 5
+            (TPriority)32,      (TPriority)0,       (TPriority)1,       (TPriority)12,
+            (TPriority)2,       (TPriority)6,       (TPriority)0xFF,    (TPriority)13,
+            (TPriority)3,       (TPriority)0xFF,    (TPriority)7,       (TPriority)0xFF,
+            (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)14,
+            (TPriority)10,      (TPriority)4,       (TPriority)0xFF,    (TPriority)0xFF,
+            (TPriority)8,       (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)25,
+            (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,
+            (TPriority)0xFF,    (TPriority)21,      (TPriority)27,      (TPriority)15,
+            (TPriority)31,      (TPriority)11,      (TPriority)5,       (TPriority)0xFF,
+            (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,
+            (TPriority)9,       (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)24,
+            (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)20,      (TPriority)26,
+            (TPriority)30,      (TPriority)0xFF,    (TPriority)0xFF,    (TPriority)0xFF,
+            (TPriority)0xFF,    (TPriority)23,      (TPriority)0xFF,    (TPriority)19,
+            (TPriority)29,      (TPriority)0xFF,    (TPriority)22,      (TPriority)18,
+            (TPriority)28,      (TPriority)17,      (TPriority)16,      (TPriority)0xFF
+        #endif  // scmRTOS_PROCESS_COUNT
+    };
+}   //namespace
+#endif
