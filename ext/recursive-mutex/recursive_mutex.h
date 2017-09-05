6d7f2121dc3e53f3e3e9d9a0b2586001f75c0ba9
Sergey A. Borshch <sb-github@rrt.lv>
2017-09-05 13:23:30 +0300

    force_unlock() and force_unlock_isr() added

--- ext/recursive-mutex/recursive_mutex.h
+++ ext/recursive-mutex/recursive_mutex.h
@@ -63,17 +63,47 @@ public:
 
     void lock();
     void unlock();
+    void unlock_isr();
     bool try_lock();
     bool try_lock(timeout_t timeout);
     bool is_locked() const { TCritSect cs; return (ValueTag != 0); }
     // Unlock the mutex despite of how many times it has been locked.
     void force_unlock();
+    void force_unlock_isr();
 
 protected:
     volatile TProcessMap ProcessMap;
     volatile TProcessMap ValueTag;
     volatile size_t NestCount;
 }; 
+
+//------------------------------------------------------------------------------
+//
+inline void TRecursiveMutex::unlock_isr()
+{
+    TCritSect cs;
+
+    if ( NestCount && --NestCount == 0 )
+    {
+        ValueTag = 0;
+        resume_next_ready_isr(ProcessMap);
+    }
+}
+
+//------------------------------------------------------------------------------
+//
+inline void TRecursiveMutex::force_unlock_isr()
+{
+    TCritSect cs;
+
+    if ( 0 == NestCount )
+        return;
+    NestCount = 0;
+    ValueTag = 0;
+    resume_next_ready_isr(ProcessMap);
+}
+
+
 
 } // ns OS
 
